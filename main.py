a2ec2b819ea6a08cf32819868a1c3e5ed1c124b3
Apostolvlad <31957880+Apostolvlad@users.noreply.github.com>
2020-03-04 18:04:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from location.locations import *# manager_location
 
 def main():
     #Location() # прописывать все импортируемые в папках локациях, либо так..., ибо он её цыпляет из них, и выдает предупреждение.. раздражает
-    g = Bot('18dc699f66a4d77a5185ea1a474c458c5a8407fcbebc47c90f3ce1139e299aa03042acc56d43e1eb7290e', '179475703')
+    g = Bot('', '') # токен группы для работы с сообщениями, и ид группы.
 
     Register(g)
     Register1(g)
